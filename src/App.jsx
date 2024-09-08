5f433cfbc4449e9112024e2c85e46f47314cca97
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-09-08 18:35:10 +0100

    Navbar implementation �

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router} from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+const App = () => {
+    return (
+        <main className='bg-slate-300/20'>
+            <Router>
+                <Navbar />
+                {/* <Routes>
+                    <Route path='/' element={'Home'} />
+                    <Route path='/about' element={'About'} />
+                    <Route path='/projects' element={'Projects'} />
+                    <Route path='/contact' element={'Contact'} />
+                </Routes> */}
+            </Router>
+        </main>
+    );
+}
+
+export default App;

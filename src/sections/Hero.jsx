2a8aeb86aaa3ff9dcdbc1d96199a208f050b4fc7
Adeosun Oluwaseyi <deosundeola@gmail.com>
2024-09-11 13:41:53 +0100

    sections

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,81 @@
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera as ThreePerspectiveCamera } from "@react-three/drei";
+import Hacks from "../components/Hacks.jsx";
+import { Suspense } from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import Target from "../components/Target.jsx";
+import Circle from "../components/Circle.jsx";
+import Eyes from "../components/Eyes.jsx";
+import Cube from "../components/Cube.jsx";
+import Rings from "../components/Rings.jsx";
+import CameraZone from "../components/Camera.jsx";
+import Button from "../components/Button.jsx";
+
+const Hero = () => {
+  const isSmall = useMediaQuery({ query: "(max-width: 440px)" });
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const isTable = useMediaQuery({
+    query: "(min-width: 768px, max-width: 1024px)",
+  });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTable);
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+          Hello, I&apos;m Oluwaseyi <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">I build websites & APIs</p>
+      </div>
+
+      <div className="w-full h-full absolute inset-0">
+        <Canvas
+          className="w-full h-full"
+          shadows
+          gl={{ antialias: false }}
+          dpr={[1, 1.5]}
+          camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}
+        >
+          <Suspense fallback={<CanvasLoader />}>
+            <ThreePerspectiveCamera makeDefault position={[0, 0, 20]} />
+
+            <CameraZone  isMobile={isMobile}>
+            <Hacks
+                position={sizes.deskPosition}
+                scale={sizes.deskScale}
+                rotation={[0, -Math.PI, 0]}
+              />
+            </CameraZone>
+
+
+            <group>
+              <Target position={sizes.targetPosition} />
+              <Circle
+                position={sizes.circlePosition}
+                rotation={sizes.circleRotation}
+                scale={sizes.circleScale}
+              />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} scale={sizes.ringScale} />
+              <Eyes position={sizes.eyePosition} />
+            </group>
+
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+        <a href="#contact" className="w-fit">
+        <Button name="Let's work togther" isBeam containerClass="sm:w-fit w-full sm:min-w-96"/>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { navLinks } from "../constants/index.js";
+
+const NavComponent = () => {
+  return (
+    <ul className="nav-ul">
+      {navLinks.map(({ id, href, name }) => (
+        <li key={id} className="nav-li">
+          <a href={href} className="nav-li_a" onClick={() => {}}>
+            {name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen);
+
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="max-w-7xl mx-auto">
+        <div className="flex justify-between item-center py-5 mx-auto c-space">
+          <a
+            href="/"
+            className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+          >
+            🐦‍🔥Oluwaseyi Adeosun
+          </a>
+          <button
+            onClick={toggleMenu}
+            className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex"
+            aria-label="Toggle menu"
+          >
+            <img
+              src={isOpen ? "assets/close.svg" : "assets/menu.svg"}
+              alt="toggle"
+              className="w-6 h-6"
+            />
+          </button>
+          <nav className="sm:flex hidden">
+            <NavComponent />
+          </nav>
+        </div>
+      </div>
+      <div className={`nav-sidebar ${isOpen ? "max-h-screen" : "max-h-0"}`}>
+        <nav className="p-5">
+          <NavComponent />
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
